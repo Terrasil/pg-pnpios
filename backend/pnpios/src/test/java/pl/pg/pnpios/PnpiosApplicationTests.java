e044af5813a1e6bc6526df0b823f55bd69a0450d
Terrasil <gormit7@gmail.com>
2026-03-27 21:05:24 +0100

    Init backend project

---
+++ backend/pnpios/src/test/java/pl/pg/pnpios/PnpiosApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.pg.pnpios;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PnpiosApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
