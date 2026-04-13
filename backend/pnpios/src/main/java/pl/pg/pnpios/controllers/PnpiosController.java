7d16a76bfef4f2b1c3084eb73385eff0ede03fe8
Terrasil <gormit7@gmail.com>
2026-04-13 12:27:21 +0200

    Init frontend app part

---
+++ backend/pnpios/src/main/java/pl/pg/pnpios/controllers/PnpiosController.java
@@ -0,0 +1,9 @@
+package pl.pg.pnpios.controllers;
+
+import lombok.AllArgsConstructor;
+import org.springframework.ws.server.endpoint.annotation.Endpoint;
+
+@Endpoint
+@AllArgsConstructor
+public class PnpiosController {
+}
---
+++ frontend/pnpios/android/app/src/main/kotlin/com/example/pnpios/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.pnpios
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
